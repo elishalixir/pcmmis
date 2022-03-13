4dc0bc91a0c7854c754f035726032721aee62cec
elishalixir <66981482+elishalixir@users.noreply.github.com>
2022-03-13 20:38:31 +0100

    front page

--- dpcmmis/views.py
+++ dpcmmis/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'dpcmmis/home.html')
+    return render(request, 'home.html')
 
 def testpage(request):
     return HttpResponse('Test PAGE')

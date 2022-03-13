9289bf0d7edab52754caaac55cc8dd352bab7cac
elishalixir <66981482+elishalixir@users.noreply.github.com>
2022-03-13 23:01:31 +0100

    model customization

--- dpcmmis/models.py
+++ dpcmmis/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class UserData(models.Model):
+    Name_of_party = models.CharField(max_length=50)
+    Industry = models.TextField(max_length=50)
+    Date_on_which_its_instrument_of_ratification = models.DateTimeField(
+        auto_now_add=False)
+    Location = models.TextField(max_length=50)
+
+    def __str__(self):
+        return self.Industry
+

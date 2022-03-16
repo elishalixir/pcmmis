fb2127a0ed44cd2b6bd0cb56c0f0608ecb32d9b3
elishalixir <66981482+elishalixir@users.noreply.github.com>
2022-03-16 23:24:28 +0100

    reduce db length

--- dpcmmis/models.py
+++ dpcmmis/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class g_dpcmmis (models.Model):
-    What_improvements_would_you_like_to_see_in_the_next_version_of_mmis = models.CharField(max_length=500)
+    Any_improvements_for_the_next_version_of_mmis = models.CharField(max_length=500)
 
 class a_Organization(models.Model):
     Name = models.CharField(max_length=250)
@@ -37,18 +37,18 @@ class c_Mercury_Supply_Source(models.Model):
     Any_mercury_added_products = models.TextField(max_length=300)
     Any_measures_to_achieve_reductions_in_mercury_use = models.TextField(max_length=300)
     State_measures_taken_to_address_mercury_emission_and_releases = models.TextField(max_length=300)
-    State_estimated_yearly_amount_of_mercury_or_mercury_compounds_used = models.TextField(max_length=300)
-    Any_measures_to_restrict_the_use_of_mercury_or_mercury_products = models.TextField(max_length=300)
+    State_estimated_yearly_amount_of_mercury_compounds_used = models.TextField(max_length=300)
+    Any_measures_to_restrict_the_use_of_mercury_products = models.TextField(max_length=300)
 
 class d_Waste(models.Model):
-    Any_interim_storage_of_non_waste_mercury_and_mercury_compounds = models.TextField(max_length=300)
+    Any_interim_storage_of_non_waste_mercury_compounds = models.TextField(max_length=300)
     Explain_how_effective_the_storage_is_environmentally = models.TextField(max_length=300)
-    Any_facility_for_final_disposal_of_mercury_or_mercury_compound_waste = models.TextField(max_length=300)
+    Any_facility_for_final_disposal_of_mercury_compound_waste = models.TextField(max_length=300)
 
 class e_Health(models.Model):
-    State_measures_taken_to_inform_the_public_on_exposure_to_mecury = models.TextField(max_length=300)
-    State_measures_taken_to_protect_human_health = models.TextField(max_length=300)
+    Any_measures_taken_to_inform_the_public_on_exposure_to_mecury = models.TextField(max_length=300)
+    Any_measures_taken_to_protect_human_health = models.TextField(max_length=300)
 
 class f_Challenges(models.Model):
-    State_the_possible_challenges_in_meeting_the_objective_of_the_convention = models.TextField(max_length=300)
+    Any_possible_challenges_in_meeting_convention_objectives = models.TextField(max_length=300)
 
